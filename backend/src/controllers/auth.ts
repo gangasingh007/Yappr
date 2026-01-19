ba0d3fa33ab8174bb80c5258e31a1feab95083b8
Ganga Singh <gangasingh1734@gmail.com>
2026-01-19 17:57:02 +0530

    feat: update backend to use environment variable for PORT and add Prisma configuration
    - Updated server to listen on a port defined in environment variables or default to 3000.
    - Added Prisma configuration file and schema for PostgreSQL.
    - Created initial migration SQL for User, Chat, and Message tables with foreign key constraints.
    - Implemented user authentication routes and middleware using Clerk.
    - Added Prisma client setup with PostgreSQL adapter.
    - Updated package.json to include new dependencies for Clerk and Prisma.
    - Created seed file for database initialization.
    - Removed unused CSS file and updated App component to use Clerk for authentication.

---
+++ backend/src/controllers/auth.ts
@@ -0,0 +1,45 @@
+import type { Response } from "express";
+import { prisma } from "../lib/prisma";
+import type { AuthenticatedRequest } from "../middleware/auth";
+import { clerkClient, getAuth } from "@clerk/express";
+
+
+export async function getMe(req: AuthenticatedRequest, res: Response) {
+  try {
+    const { userId } = req;
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export const getCallback = async (req: AuthenticatedRequest, res: Response) => {
+  try {
+    const {userId : clerkId}  = getAuth(req);
+    if (!clerkId) {
+      return res.status(400).json({ message: "Unauthorized" });
+    }
+    let user = await prisma.user.findFirst({
+      where: { clerkId },
+    });
+    
+    if (!user) {
+    const clerUser = await clerkClient.users.getUser(clerkId);
+    user = await prisma.user.create({
+        data: {
+            clerkId: clerUser.id,
+            email: clerUser.emailAddresses[0]?.emailAddress || "",
+            fullName: clerUser.firstName + " " + clerUser.lastName,
+            avatar: clerUser.imageUrl,
+        }
+    })
+    }
+    res.json(user);
+  } catch (error : any) {
+    console.log(error.message)
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
